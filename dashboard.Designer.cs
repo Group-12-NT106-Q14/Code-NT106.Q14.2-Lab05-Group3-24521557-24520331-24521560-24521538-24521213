b91410758f7e992fc3f9c17adf82b9ba84ad3da6
thnguyen290106 <thnguyen290106@gmail.com>
2025-12-11 13:50:08 +0700

    Added Bai01

---
+++ dashboard.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Code_NT106.Q14._2_Lab05_Group3_24521557_24520331_24521560_24521538_24521213
+{
+    partial class dashboard
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            btnBai06 = new Button();
+            btnBai05 = new Button();
+            btnBai04 = new Button();
+            btnBai03 = new Button();
+            btnBai02 = new Button();
+            btnBai01 = new Button();
+            lblTitle = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Navy;
+            label1.Location = new Point(133, 57);
+            label1.Name = "label1";
+            label1.Size = new Size(331, 35);
+            label1.TabIndex = 15;
+            label1.Text = "Nhóm 3 - NT106.Q14.2";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // btnBai06
+            // 
+            btnBai06.BackColor = Color.LightBlue;
+            btnBai06.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnBai06.ForeColor = Color.White;
+            btnBai06.Location = new Point(427, 235);
+            btnBai06.Margin = new Padding(3, 4, 3, 4);
+            btnBai06.Name = "btnBai06";
+            btnBai06.Size = new Size(150, 75);
+            btnBai06.TabIndex = 14;
+            btnBai06.Text = "Bài 06";
+            btnBai06.UseVisualStyleBackColor = false;
+            // 
+            // btnBai05
+            // 
+            btnBai05.BackColor = Color.LightBlue;
+            btnBai05.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnBai05.ForeColor = Color.White;
+            btnBai05.Location = new Point(227, 235);
+            btnBai05.Margin = new Padding(3, 4, 3, 4);
+            btnBai05.Name = "btnBai05";
+            btnBai05.Size = new Size(150, 75);
+            btnBai05.TabIndex = 13;
+            btnBai05.Text = "Bài 05";
+            btnBai05.UseVisualStyleBackColor = false;
+            // 
+            // btnBai04
+            // 
+            btnBai04.BackColor = Color.LightBlue;
+            btnBai04.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnBai04.ForeColor = Color.White;
+            btnBai04.Location = new Point(27, 235);
+            btnBai04.Margin = new Padding(3, 4, 3, 4);
+            btnBai04.Name = "btnBai04";
+            btnBai04.Size = new Size(150, 75);
+            btnBai04.TabIndex = 12;
+            btnBai04.Text = "Bài 04";
+            btnBai04.UseVisualStyleBackColor = false;
+            // 
+            // btnBai03
+            // 
+            btnBai03.BackColor = Color.LightBlue;
+            btnBai03.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnBai03.ForeColor = Color.White;
+            btnBai03.Location = new Point(427, 110);
+            btnBai03.Margin = new Padding(3, 4, 3, 4);
+            btnBai03.Name = "btnBai03";
+            btnBai03.Size = new Size(150, 75);
+            btnBai03.TabIndex = 11;
+            btnBai03.Text = "Bài 03";
+            btnBai03.UseVisualStyleBackColor = false;
+            // 
+            // btnBai02
+            // 
+            btnBai02.BackColor = Color.LightBlue;
+            btnBai02.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnBai02.ForeColor = Color.White;
+            btnBai02.Location = new Point(227, 110);
+            btnBai02.Margin = new Padding(3, 4, 3, 4);
+            btnBai02.Name = "btnBai02";
+            btnBai02.Size = new Size(150, 75);
+            btnBai02.TabIndex = 10;
+            btnBai02.Text = "Bài 02";
+            btnBai02.UseVisualStyleBackColor = false;
+            // 
+            // btnBai01
+            // 
+            btnBai01.BackColor = Color.LightBlue;
+            btnBai01.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnBai01.ForeColor = Color.White;
+            btnBai01.Location = new Point(27, 110);
+            btnBai01.Margin = new Padding(3, 4, 3, 4);
+            btnBai01.Name = "btnBai01";
+            btnBai01.Size = new Size(150, 75);
+            btnBai01.TabIndex = 9;
+            btnBai01.Text = "Bài 01";
+            btnBai01.UseVisualStyleBackColor = false;
+            btnBai01.Click += btnBai01_Click;
+            // 
+            // lblTitle
+            // 
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new Font("Arial", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTitle.ForeColor = Color.Navy;
+            lblTitle.Location = new Point(47, 9);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(511, 35);
+            lblTitle.TabIndex = 8;
+            lblTitle.Text = "Lab 05 - Gửi và Nhận Mail trong C#";
+            lblTitle.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // dashboard
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(613, 387);
+            Controls.Add(label1);
+            Controls.Add(btnBai06);
+            Controls.Add(btnBai05);
+            Controls.Add(btnBai04);
+            Controls.Add(btnBai03);
+            Controls.Add(btnBai02);
+            Controls.Add(btnBai01);
+            Controls.Add(lblTitle);
+            Name = "dashboard";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button btnBai06;
+        private Button btnBai05;
+        private Button btnBai04;
+        private Button btnBai03;
+        private Button btnBai02;
+        private Button btnBai01;
+        private Label lblTitle;
+    }
+}
---
+++ dashboard.cs
@@ -0,0 +1,18 @@
+﻿namespace Code_NT106.Q14._2_Lab05_Group3_24521557_24520331_24521560_24521538_24521213
+{
+    public partial class dashboard : Form
+    {
+        public dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBai01_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Bai01 bai01 = new Bai01();
+            bai01.ShowDialog();
+            this.Show();
+        }
+    }
+}
